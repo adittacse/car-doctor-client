633b550cf48d24e9a1b695ee88215934a2685818
Aditta Chakraborty <adittacse@gmail.com>
2023-05-16 02:18:50 +0600

    implemented user observer

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, {createContext, useState} from 'react';
-import { getAuth, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import React, {createContext, useEffect, useState} from 'react';
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import app from "../firebase/firbase.config.js";
 
 export const AuthContext = createContext(null);
@@ -10,12 +10,24 @@ const AuthProvider = ({ children }) => {
     const [loading, setLoading] = useState(true);
     
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
     
     const logOut = () => {
         return signOut(auth);
     }
+    
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+        
+        return () => {
+            unsubscribe();
+        }
+    },[]);
     
     const authInfo = {
         user,
