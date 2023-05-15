fbe09ddd28597a714a650a80bc65a9d1e790d607
Aditta Chakraborty <adittacse@gmail.com>
2023-05-15 17:28:15 +0600

    about us section added in home page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from "../Banner/Banner.jsx";
+import About from "../About/About.jsx";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About></About>
         </div>
     );
 };
