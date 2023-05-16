8d66c27bb4428dd8f136306b367b2c548a47b6f2
Aditta Chakraborty <adittacse@gmail.com>
2023-05-17 01:41:03 +0600

    booked service table inut button and status button updated

--- src/Pages/Bookings/BookingRow.jsx
+++ src/Pages/Bookings/BookingRow.jsx
@@ -6,9 +6,9 @@ const BookingRow = ({ booking }) => {
     return (
         <tr>
             <th>
-                <label>
-                    <input type="checkbox" className="checkbox" />
-                </label>
+                <button className="btn btn-circle btn-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </th>
             <td>
                 <div className="flex items-center space-x-3">
@@ -31,7 +31,7 @@ const BookingRow = ({ booking }) => {
             </td>
             <td>{date}</td>
             <th>
-                <button className="btn btn-ghost btn-xs">Pending</button>
+                <button className="btn bg-error text-white">Pending</button>
             </th>
         </tr>
     );
